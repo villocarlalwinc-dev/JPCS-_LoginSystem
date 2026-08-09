1424f1770e25155755acae058da9467ad53b3390
Alwin <villocarlalwinc@gmail.com>
2026-08-09 14:25:39 +0800

    Create DataStore class for user and announcement management
    Introduced DataStore class to manage users and announcements.

---
+++ Models/DataStore.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+namespace JPCS.Models
+{
+    public static class DataStore
+    {
+        public static List<User> Users { get; set; } = new List<User>();
+
+        public static List<Announcement> Announcements { get; set; } = new List<Announcement>
+        {
+            new Announcement
+            {
+                Id = 1,
+                Title = "Welcome!",
+                Content = "Welcome to the LSHCG Student Portal.",
+                DatePosted = new DateTime(2026, 8, 1)
+            },
+            new Announcement
+            {
+                Id = 2,
+                Title = "Enrollment Update",
+                Content = "Enrollment for next semester starts soon.",
+                DatePosted = new DateTime(2026, 8, 5)
+            }
+        };
+
+        private static int _nextUserId = 1;
+
+        public static int GetNextUserId() => _nextUserId++;
+    }
+}
